628c99486e1a2e3e8d620383579604b2b55098dd
dak98 <jakub.czajka1998@gmail.com>
2019-10-22 18:20:37 +0200

    Connected the frequency graph to the audio input

--- include/frequency_graph.hpp
+++ include/frequency_graph.hpp
@@ -8,19 +8,64 @@
 #include <QtCharts/QValueAxis>
 QT_CHARTS_USE_NAMESPACE
 
+#include <QtCore/QTimer>
+
+#include <QtMultimedia/QAudioDeviceInfo>
+#include <QtMultimedia/QAudioInput>
+
+#include <memory>
+
 namespace wiet_star
 {
+
+struct freq_graph_config
+{
+    qreal min_frequency;
+    qreal max_frequency;
+
+    qreal min_amplitude;
+    qreal max_amplitude;
+};
+
+struct input_audio_config
+{
+    QString device_name;
+    int channel_count;
+
+    int sample_rate;
+    int sample_size;
+    QAudioFormat::SampleType sample_type;
+
+    QString codec;
+    QAudioFormat::Endian byte_order;
+
+    int buffer_size;
+};
 
 class frequency_graph : public QChartView
 {
     Q_OBJECT
 public:
-    frequency_graph();
+    frequency_graph(freq_graph_config const& graph_config,
+                    input_audio_config const& audio_config);
+
+    ~frequency_graph();
+
+private slots:
+    void refresh_graph();
 
 private:
     QChart main_graph;
     QLineSeries main_series;
     QValueAxis x_axis, y_axis;
+
+    QAudioDeviceInfo input_device;
+    QAudioFormat input_audio_format;
+    std::unique_ptr<QAudioInput> input_handle;
+
+    std::unique_ptr<QIODevice> input_buffer;
+
+    std::unique_ptr<QTimer> refresh_timer;
 };
 
 } // wiet_star
--- src/frequency_graph.cpp
+++ src/frequency_graph.cpp
@@ -1,15 +1,25 @@
 #include "frequency_graph.hpp"
 
+#include <QtCore/QDebug>
+
+#include <system_error>
+#include <thread>
+#include <unistd.h>
+
+#include <iostream>
+
 namespace wiet_star
 {
 
-frequency_graph::frequency_graph()
+frequency_graph::frequency_graph(freq_graph_config const& graph_config,
+                                 input_audio_config const& audio_config)
+    : refresh_timer(new QTimer(this))
 {
-    x_axis.setTitleText("Częstotliwości");
-    x_axis.setRange(20000, 40000);
+    x_axis.setTitleText("Częstotliwość");
+    x_axis.setRange(graph_config.min_frequency, graph_config.max_frequency);
 
     y_axis.setTitleText("Amplituda");
-    y_axis.setMin(-1);
+    y_axis.setRange(graph_config.min_amplitude, graph_config.max_amplitude);
 
     main_graph.addSeries(&main_series);
     main_graph.legend()->hide();
@@ -18,6 +28,78 @@ frequency_graph::frequency_graph()
     main_graph.setTitle("Wynik transformaty Fouriera");
 
     setChart(&main_graph);
+
+    QList<QAudioDeviceInfo> devices = QAudioDeviceInfo::availableDevices(QAudio::AudioInput);
+    bool device_not_found = true;
+    for (auto it = std::begin(devices); it != std::end(devices) && device_not_found; ++it)
+        if (it->deviceName() == audio_config.device_name)
+        {
+            input_device = *it;
+            device_not_found = false;
+        }
+
+    if (input_device.isNull())
+    {
+        input_device = QAudioDeviceInfo::defaultInputDevice();
+        qDebug() << "Device " + audio_config.device_name + " not found. Using " +
+                    "the default one";
+    }
+
+    input_audio_format.setChannelCount(audio_config.channel_count);
+    input_audio_format.setSampleRate(audio_config.sample_rate);
+    input_audio_format.setSampleSize(audio_config.sample_size);
+    input_audio_format.setSampleType(audio_config.sample_type);
+    input_audio_format.setCodec(audio_config.codec);
+    input_audio_format.setByteOrder(audio_config.byte_order);
+
+    if (!input_device.isFormatSupported(input_audio_format))
+    {
+        qWarning() << "Default format not supported, trying to use the nearest.";
+        input_audio_format = input_device.nearestFormat(input_audio_format);
+    }
+
+    input_handle = std::unique_ptr<QAudioInput>(new QAudioInput(input_device, input_audio_format));
+    if (input_handle->error() != QAudio::NoError)
+    {
+        qDebug() << "QAudioInput error: " << input_handle->error();
+        throw std::system_error(std::make_error_code(std::errc::io_error));
+    }
+
+    input_buffer = std::unique_ptr<QIODevice>(input_handle->start());
+    if (input_handle->error() != QAudio::NoError)
+    {
+        qDebug() << "QAudioInput#start error: " << input_handle->error();
+        throw std::system_error(std::make_error_code(std::errc::io_error));
+    }
+
+    connect(refresh_timer.get(), &QTimer::timeout, this, &frequency_graph::refresh_graph);
+    refresh_timer->start(5000);
+}
+
+frequency_graph::~frequency_graph()
+{
+    refresh_timer->stop();
+    input_handle->stop();
+    input_buffer->close();
+}
+
+void frequency_graph::refresh_graph()
+{
+    char audio_data[100];
+    if (-1 == input_buffer->read(audio_data, 100))
+    {
+        std::cout << audio_data << std::endl;
+    }
+
+    qreal step = (x_axis.max() - x_axis.min()) / 100;
+    QVector<QPointF> points(100);
+    for (int i = 0; i < 100; i++)
+    {
+        points[i].setX(x_axis.min() + i * step);
+        qreal y = uchar(audio_data[i]);
+        points[i].setY(y / 128);
+    }
+    main_series.replace(points);
 }
 
 } // wiet_star
